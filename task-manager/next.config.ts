21f1bb7aa88325195f05d7d014615b2611e8bdf9
Hasinijayathilaka <hasinijayathilaka5830@gmail.com>
2025-11-09 12:22:12 +0530

    Update next.config.ts

--- task-manager/next.config.ts
+++ task-manager/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // You can leave this empty or add future options
 };
 
 export default nextConfig;
